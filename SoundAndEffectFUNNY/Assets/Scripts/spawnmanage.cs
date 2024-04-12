583f76070bd8ac29b8cdd60742ddc5a780549dfa
SmallBowl <721025@student.dusd.net>
2024-04-12 15:27:05 -0700

    MEGA!!!

--- SoundAndEffectFUNNY/Assets/Scripts/spawnmanage.cs
+++ SoundAndEffectFUNNY/Assets/Scripts/spawnmanage.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class spawnmanage : MonoBehaviour
 {
     public GameObject[] obstacles;
-    
+    public GameObject[] obstaclesMega;
 
     private float startDelay = 2;
-    private float repeatRate = 2f;
-    int off = 0;
+    private float repeatRate = 2.5f;
+    
     Vector3 goog;
+    private bool on = true;
+    private int count = 0;
+    private int mega;
+    public int megacap = 20;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,22 +24,47 @@ public class spawnmanage : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
     void Spawn()
     {
-        
-        int funny = Random.Range(0, obstacles.Length);
-        if (funny >= 3)
+        if (on)
         {
-            goog = new(Random.Range(25 + off, 29 + off), 3.5f, -2);
-            off += 12;
+            mega = Random.Range(0, megacap);
+            if (mega == 0) 
+            {
+                 int meg = Random.Range(0, obstaclesMega.Length);
+                Instantiate(obstaclesMega[meg], new(30, 3.5f, -2), obstaclesMega[meg].transform.rotation);
+                megacap = 20;
+                count = 2;
+                on = false;
+            }
+            else
+            {
+                int funny = Random.Range(0, obstacles.Length);
+                if (funny >= 3)
+                {
+                    goog = new(Random.Range(28, 32), 3.5f, -2);
+                    on = false;
+                    count++;
+                }
+                else
+                {
+                    goog = new(Random.Range(26, 30), 0, 0);
+                }
+                Instantiate(obstacles[funny], goog, obstacles[funny].transform.rotation);
+            }
+           
         }
         else
         {
-            goog = new(Random.Range(23 + off, 27 + off), 0, 0);
+            count--;
+            if (count == 0)
+            {
+                on = true; 
+            }
         }
-        Instantiate(obstacles[funny], goog, obstacles[funny].transform.rotation);
+       
     }
 }
