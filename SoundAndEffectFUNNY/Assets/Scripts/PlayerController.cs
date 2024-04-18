dd2e6c89fbe0ae2e9fc5f3746c9d898e67e0022c
SmallBowl <145072338+JoshuaGarcia-SmallBowl@users.noreply.github.com>
2024-04-18 11:40:10 -0700

    Animation

--- SoundAndEffectFUNNY/Assets/Scripts/PlayerController.cs
+++ SoundAndEffectFUNNY/Assets/Scripts/PlayerController.cs
@@ -11,6 +11,8 @@ public class PlayerController : MonoBehaviour
     public bool ground = true;
     private bool dbl = true;
     public bool gameOver = false;
+    private Animator playerAnim;
+    public bool guy;
     
 
     // Start is called before the first frame update
@@ -18,24 +20,37 @@ public class PlayerController : MonoBehaviour
     {
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
+        
+        
+            playerAnim = GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && ground)
+        if (Input.GetKeyDown(KeyCode.Space) && ground && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             ground = false;
             
             dbl = true;
+            
+            
+                playerAnim.SetTrigger("Jump_trig");
+            
         }
         else if (Input.GetKeyDown(KeyCode.Space) && dbl)
         {
             playerRb.velocity = new(0,0,0);
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             dbl = false;
+            if (guy)
+            {
+                playerAnim.SetTrigger("Jump_trig");
+            }
         }
+
 
 
     }
@@ -46,6 +61,17 @@ public class PlayerController : MonoBehaviour
         {
 
             gameOver = true;
+            if (guy)
+            {
+                playerAnim.SetBool("Death_b", true);
+                playerAnim.SetInteger("DeathType_int", 1);
+            }
+            else
+            {
+                playerAnim.SetFloat("Speed_f", 0);
+                playerAnim.SetBool("Eat_b", true);
+            }
+                    
 
         }
         else
